849da1c318c0b486234c776b25de862bcef53cdb
maemeier <maemeier@live.com>
2019-08-27 01:02:49 +0200

    install bs, edit api and component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Weather from "./components/weather";
 const API_KEY = "d554b19eb3803ac4c50344d267a27ad5";
 
 class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
   getWeather = async e => {
     e.preventDefault();
     const city = e.target.elements.city.value;
@@ -15,14 +24,41 @@ class App extends React.Component {
       `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
     );
     const data = await api_call.json();
-    console.log(data);
+
+    if (city && country) {
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Don't be lazy! Enter the city and county plzzzz"
+      });
+    }
   };
   render() {
     return (
       <div>
         <Titles />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/components/weather.js
+++ src/components/weather.js
@@ -2,7 +2,26 @@ import React from "react";
 
 class Weather extends React.Component {
   render() {
-    return <div> Weather Component</div>;
+    return (
+      <div>
+        <h6>Weather Component</h6>
+        {this.props.city && this.props.country && (
+          <p>
+            Location : {this.props.city}, {this.props.country}
+          </p>
+        )}
+
+        {this.props.temperature && (
+          <p>Temperature : {this.props.temperature}</p>
+        )}
+
+        {this.props.humidity && <p>Humidity : {this.props.humidity}</p>}
+        {this.props.description && (
+          <p>Description : {this.props.description}</p>
+        )}
+        {this.props.error && <p>{this.props.error}</p>}
+      </div>
+    );
   }
 }
 export default Weather;
